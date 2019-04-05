ad0e9aa8409d1d2172ba3974d907f8886774d1fe
00140000 <00140000@st-tep-lis1-n22.labst.academico.pucmm>
2019-04-04 21:26:29 -0400

    Hasta el ejercicio 12

---
+++ TrabajoFinal/src/trabajofinal.py
@@ -0,0 +1,69 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    def nombre():
+        n = input("Digite el numero por el cual desea multiplicar")
+        name = "Franklin " * n
+        print name
+        
+    def suma():
+        n = input("Digite el numero")
+        t = 0
+        i = 0
+        while i <= n:
+            t = t + i
+            i = i + 1
+        print "el resultado de la suma es: ",t
+        
+    def imp():
+        n = 0 
+        while n <20:
+            n = n + 1
+            if n % 2 != 0:
+              print n
+              
+   
+    
+    def impd():
+        n = 20 
+        while n > 0:
+            n = n - 1
+            if n % 2 != 0:
+              print n
+        
+    
+            
+    def media():
+        n1 = float(input("inserte el primer numero"))
+        n2 = float(input ("inserte el segundo valor"))
+        media2 = (2/(1/n1 + 1/n2))
+        print media2
+        
+    
+    
+    def division():
+        n = int(input("Digite un numero para buscar divisores perfectos"))
+        if n > 0:
+            print "El parametro introducido no es aceptable"
+        
+        for nt in range(1,n+1):
+            if n % nt == 0:
+             print nt
+    
+    def enteros_positivos():
+        n1 = int(input("digite el primer numero"))
+        n2 = int(input("digite el segundo numero"))
+        
+    #Ejercicio 12    
+    def suma_sucesiva():
+        
+        
+    
+    
+        
+    
+    
+        
+    
